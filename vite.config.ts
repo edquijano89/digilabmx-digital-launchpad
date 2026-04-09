1df8ea67d792a88a68d8c711e9f272cb64bc6b66
digilabmx <43075042+digilabmx@users.noreply.github.com>
2026-04-09 10:49:55 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: '/digilabmx-digital-launchpad/',
+  base: process.env.CF_PAGES ? '/' : '/digilabmx-digital-launchpad/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
